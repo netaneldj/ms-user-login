6a46169b811030c5878e2e997077d25d672eba33
Netanel David Jamilis <23279713+netaneldj@users.noreply.github.com>
2023-04-13 09:09:50 -0300

    fix email and password regex validation

--- login/src/main/java/com/jamilis/login/dto/SignUpRequestDto.java
+++ login/src/main/java/com/jamilis/login/dto/SignUpRequestDto.java
@@ -16,12 +16,12 @@ public class SignUpRequestDto {
     private String name;
     @NotNull
     @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-z]+.[a-z]{2,6}$",
+    @Pattern(regexp = "^(.+)@(.+)[.](.+)$",
             message = "Invalid email format. Email format must be like: aaaaaaa@undominio.algo")
     private String email;
     @NotNull
     @NotEmpty
-    @Pattern(regexp = "^[A-Z]{1}[0-9]{2}[a-z]{5,9}$",
+    @Pattern(regexp = "^(?=(?:[^A-Z]*[A-Z]){1}[^A-Z]*$)(?=(?:[^0-9]*[0-9]){2}[^0-9]*$).{8,12}$",
             message = "Password must contain 1 uppercase, 2 digits and lowercases. Must have length between 8 and 12")
     private String password;
     private List<PhoneDto> phones;
